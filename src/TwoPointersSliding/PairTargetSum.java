d26f36d41e055e80d28a89eae6b4282bc0c61dd5
viku01999 <viku01999@gmail.com>
2026-04-05 00:23:02 +0530

    two pointers

---
+++ src/TwoPointersSliding/PairTargetSum.java
@@ -0,0 +1,29 @@
+package TwoPointersSliding;
+
+public class PairTargetSum {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int target = 6;
+
+        int left = 0;
+        int right = arr.length - 1;
+        int countPairs = 0;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println("Found: " + arr[left] + ", " + arr[right]);
+                countPairs += 1;
+//                right--;
+//                left++;
+                break;
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        System.out.println(countPairs);
+    }
+}
